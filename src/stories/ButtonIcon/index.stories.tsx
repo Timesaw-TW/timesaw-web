f293af91e1573d57b42ab494ae8c4554338dadfb
Leo Liao <honglin0822@outlook.com>
2024-02-23 21:10:45 +0800

    :sparkles: Add ButtonIcon storybook component

---
+++ src/stories/ButtonIcon/index.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import ButtonIcon from ".";
+import IconTrashOutline from "../Icons/IconTrashOutline";
+import IconTrashSolid from "../Icons/IconTrashSolid";
+
+const meta = {
+  title: "Base/Button Icon",
+  component: ButtonIcon,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof ButtonIcon>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Trash_Outline: Story = {
+  args: {
+    label: "Delete",
+    children: <IconTrashOutline />,
+  },
+};
+
+export const Trash_Solid: Story = {
+  args: {
+    label: "Delete",
+    children: <IconTrashSolid />,
+  },
+};
