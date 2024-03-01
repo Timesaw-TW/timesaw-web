2a5dcedf95364a36db35a254a86b13d5efb1d6d1
Leo Liao <honglin0822@outlook.com>
2024-03-01 23:33:14 +0800

    :bug: Change next/font from online to local

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,10 +12,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        main: ["var(--font-notoSans)"],
       },
       colors: {
         primary: {
