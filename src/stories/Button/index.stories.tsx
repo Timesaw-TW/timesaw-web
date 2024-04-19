f54252cf496ca50d5137df30c53975e350368f52
Leo Liao <honglin0822@outlook.com>
2024-04-20 02:16:22 +0800

    :bug: Fix Button default size and color

--- src/stories/Button/index.stories.tsx
+++ src/stories/Button/index.stories.tsx
@@ -14,17 +14,42 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Sample: Story = {
-  name: "Button",
+export const Sample_1: Story = {
+  name: "Button Primary/Soda Blue/80%",
   args: {
     children: "Button",
+    className: "!bg-soda-80",
   },
 };
 
-export const Sample_Light: Story = {
-  name: "Button with light color",
+export const Sample_2: Story = {
+  name: "Button Primary/Soda Blue/100%",
   args: {
     children: "Button",
-    className: "bg-soda-20",
+    className: "!bg-soda-100",
+  },
+};
+
+export const Sample_3: Story = {
+  name: "Button Primary/Soda Blue/60%",
+  args: {
+    children: "Button",
+    className: "!bg-soda-60",
+  },
+};
+
+export const Sample_4: Story = {
+  name: "Button Primary/Soda Blue/40%",
+  args: {
+    children: "Button",
+    className: "!bg-soda-40",
+  },
+};
+
+export const Sample_5: Story = {
+  name: "Button Primary/Soda Blue/20%",
+  args: {
+    children: "Button",
+    className: "!bg-soda-20",
   },
 };
