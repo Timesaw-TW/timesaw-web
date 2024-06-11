7b8e6401cc97dd11885094b85c44f7d8f7a6bdfb
Leo Liao <honglin0822@outlook.com>
2024-06-12 01:33:32 +0800

    :lipstick: Fix login page style

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -9,33 +9,34 @@ interface Props {
 
 const LoginLayout: FC<Props> = ({ children }) => {
   return (
-    <div className="flex h-full items-end justify-center bg-soda-20">
-      <div className={merge("relative", "w-full sm:w-[42.5rem]")}>
+    <div className="flex h-full items-end justify-center overflow-x-hidden bg-soda-20">
+      <div className={merge("relative h-full", "w-full lg:w-[42.5rem]")}>
         <IconLogoText
           className={merge(
             "absolute fill-soda-40",
-            "-right-2 -top-[calc(3.25rem+8rem)] h-[3.25rem] w-[17.5rem]",
-            "sm:-top-[calc(5.25rem+5.5rem)] sm:h-[5.25rem] sm:w-[27rem]",
-            "lg:-right-0 lg:-top-[calc(5.25rem+5rem)] lg:w-[27.5rem]"
+            "-right-2 bottom-[calc(70%+8rem)] z-20 h-[3.25rem] w-[17.5rem]",
+            "sm:bottom-[calc(75%+5.5rem)] sm:h-[5.25rem] sm:w-[27rem]",
+            "lg:-right-0 lg:bottom-[calc(75%+5rem)] lg:w-[27.5rem]"
           )}
         />
         <Tim
           className={merge(
             "absolute fill-soda-60",
-            "-left-4 -top-[calc(11.125rem/3)*2] h-[11.125rem] w-[10.125rem]",
-            "sm:-top-[calc(13.5rem/3)*2] sm:left-4 sm:h-[13.5rem] sm:w-[12.25rem]",
+            "-left-4 bottom-[calc(70%-(11.125rem/3))] h-[11.125rem] w-[10.125rem]",
+            "sm:bottom-[calc(75%-(13.5rem/3))] sm:left-4 sm:h-[13.5rem] sm:w-[12.25rem]",
             "lg:w-[12.5rem]"
           )}
         />
         <div
           className={merge(
-            "flex items-center justify-center",
-            "relative z-[1] h-[75vh]",
-            "rounded-t-2xl  bg-white",
-            "border-l-2 border-r-2 border-t-2 border-soda-100"
+            "flex items-start justify-center",
+            "absolute w-full",
+            "top-[30%] sm:top-[25%]",
+            "rounded-t-2xl bg-white sm:rounded-2xl",
+            "border-l-2 border-r-2 border-t-2 border-soda-100 sm:border-2"
           )}
         >
-          {children}
+          <div className="relative z-[1]">{children}</div>
         </div>
       </div>
     </div>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,18 @@
 import { merge } from "@/libs/tailwind";
 import LoginBox from "@/components/auth/login/LoginBox";
+import { Suspense } from "react";
 
 const LoginPage = () => {
   return (
-    <LoginBox
-      className={merge("sm:w-[30rem] lg:w-[22.5rem]", "px-4 pt-6 lg:px-0")}
-    />
+    <Suspense>
+      <LoginBox
+        className={merge(
+          "sm:w-[30rem] lg:w-[22.5rem]",
+          "px-4 lg:px-0",
+          "sx:py-10 py-6"
+        )}
+      />
+    </Suspense>
   );
 };
 
